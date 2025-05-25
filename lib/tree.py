eaf967722034ec978e7ae4c0e34d1551ef554674
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-25 18:37:52 +0300

    implements tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,41 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self.depth_first_search(self.root, id)
+  
+  def depth_first_search(self, node, id):
+    if node["id"] == id:
+      return node
+    
+    for child in node["children"]:
+      result = self.depth_first_search(child, id)
+      if result: return result
+
+    return None
+  
+
+root = {
+    "tag_name": "div",
+    "id": "container",
+    "text_content": "",
+    "children": [
+      {
+            "tag_name": "h1",
+            "id": "heading",
+            "text_content": "Hello",
+            "children": []
+      },
+      {
+            "tag_name": "p",
+            "id": "intro",
+            "text_content": "Welcome",
+            "children": []
+      }
+    ]
+  }
+
+
+tree = Tree(root)
+print(tree.get_element_by_id("intro"))
+
+    
